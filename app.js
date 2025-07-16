ffa94b0a47e5e099abd133f8f14daf006fa32b68
dusa_sai_krishna <dsai9441695421@gmail.com>
2025-07-16 15:12:36 +0530

    Final commit

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-console.log("Some line got updated")
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(3000);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
